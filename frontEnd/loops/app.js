4542604f31641557ea64e6b11a9e43b8b5e2df0e
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-23 15:46:17 +0500

    Update index.html and style.css

---
+++ frontEnd/loops/app.js
@@ -0,0 +1,20 @@
+// const FavMovie="SpiderMan";
+
+// let guess =prompt("Guess my favourite Movie Dude...!")
+
+// while( guess!="SpiderMan" && (guess!="quit")){
+//         guess=prompt("Wrong guess. please try again")
+// }
+
+// if(gues==FavMovie){
+//         console.log("Congrats");
+
+// }
+// else{
+//         console.log("better luck next time")
+// }
+let i=0;
+while(i<=24){
+        console.log(i)
+        i++;
+}
