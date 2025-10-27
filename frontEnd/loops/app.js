0e5fb2c5b8b4ee6038d65a8086c81881921d9548
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-27 11:29:26 +0500

    Update index.html and style.css

--- frontEnd/loops/app.js
+++ frontEnd/loops/app.js
@@ -1,6 +1,21 @@
-let man={
-        name:"hulk",
-        cast:"hero",
-        role:"villain",
-        jersyNo:234
-};
+const max=prompt("enter your max number");
+
+const random=Math.floor(Math.random()*max)+1;
+
+let guess =
+prompt("guess the number ");
+while (true){
+        if(guess=="quit"){
+                console.log("user has been quit the game");
+                break;
+        }
+        if(guess==random){
+                console.log("you are right : congrats! random number was ",random);
+        }
+        else if (guess<random){
+                guess=prompt("hint: your guess ");
+
+        }else {
+                guess=prompt("hint:your guess was too large. please try again");
+        }
+}
