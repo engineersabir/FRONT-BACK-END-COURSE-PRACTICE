1a8160a636bbf991cefa8e1051e5735fad1c74b9
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-13 21:48:46 +0500

    Update index.html and style.css

--- frontEnd/loops/app.js
+++ frontEnd/loops/app.js
@@ -1,4 +1,10 @@
-function sum(a,b=3){
-        return a+b;
-}
-sum(2);
+let arr=[1,2,3,4,5,6,7,8]
+let newArr=[...arr];
+
+let chars=[..."hello"];
+
+let odd=[1,3,5,7,9];
+
+let even=[2,4,6,8,10];
+
+let nums=[...even, ...odd];
