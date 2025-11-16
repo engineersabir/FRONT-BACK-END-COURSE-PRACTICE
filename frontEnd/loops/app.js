99dcd0750318147c14c5861a686170971de8d01c
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-16 16:57:08 +0500

    Update index.html and style.css

--- frontEnd/loops/app.js
+++ frontEnd/loops/app.js
@@ -1,10 +1,23 @@
-let btn=document.querySelector("button");
+let btn =document.querySelector("button");
 
-for (btns of btn){
-    btn.onclick=sayHello;
-    console.dir(btns);
-}
+btn.addEventListener("click", function(){
+    let h3=document.querySelector("h3");
+    
+    
+    let randomColor=getRandomColor;
 
-function sayHello(){
-    alert("hello");
+    h3.innerText=randomColor;
+
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
+});
+
+function getRandomColor(){
+    let red =Math.floor(Math.random()*255);
+    let green =Math.floor(Math.random()*255);
+    let blue =Math.floor(Math.random()*255);
+
+    let color=`rgb${red}, ${green}, ${blue}`;
+    return color;
+
 }
