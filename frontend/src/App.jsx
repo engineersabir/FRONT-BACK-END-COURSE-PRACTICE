9efaa7bd7a01196dc173877cd58b91feb2e3965f
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-28 19:55:04 +0500

    Update index.html and style.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import './App.css'
-import {MovieCard}  from './components/movieCard';
-function App() {
-  return (
-    <>
-
-<MovieCard movie={{title:"tim's", release_date:"2025"}} />
+function MovieCard({movie}){
 
 
-    
-</>
-)
+    function onFavoriteClick(){
+alert("clicked");
+    }
+    return <div className="movie-card">
+    <div className="movie-poster">
+        <img src={movie.url} alt={movie.title}/>
+        <div className="movie-overlay">
+        <button className="favorite-btn" onClick={onFavoriteClick}></button>
+
+        </div>
+    </div>
+
+    <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+    </div>
+
+    </div>
 }
 
-export default App
+export default MovieCard
+
+
+
+
+
+
--- frontend/src/components/movieCard.jsx
+++ frontend/src/components/movieCard.jsx
@@ -1,30 +1,16 @@
-function MovieCard({movie}){
+import './App.css'
+import {MovieCard}  from './components/movieCard';
+function App() {
+  return (
+    <>
+
+<MovieCard movie={{title:"tim's", release_date:"2025"}} />
+<MovieCard movie={{title:"Joe's", release_date:"2026"}} />
 
 
-    function onFavoriteClick(){
-alert("clicked");
-    }
-    return <div className="movie-card">
-    <div className="movie-poster">
-        <img src={movie.url} alt={movie.title}/>
-        <div className="movie-overlay">
-        <button className="favorite-btn" onClick={onFavoriteClick}></button>
-
-        </div>
-    </div>
-
-    <div className="movie-info">
-        <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
-    </div>
-
-    </div>
+    
+</>
+)
 }
 
-export default MovieCard
-
-
-
-
-
-
+export default App
