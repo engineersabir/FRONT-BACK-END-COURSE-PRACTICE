6ba4e9124ae754406edb909d8e1487edcdb1b224
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-30 00:11:50 +0500

    Update index.html and style.css

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -1,17 +1,28 @@
 import MovieCard from "../src/App"
+import { useState } from "react ";
+
 
 function Home(){
+
+    const [searchQuery, setSearchQuery]=useState("");
     const movies=[
         {id:1, title:"John Wick", release_date:"2023"},
         {id:1, title:"John hilton", release_date:"2020"},
         {id:1, title:"Michael Wick", release_date:"2013"},
     ];
 
-    const handleSearch=()=>{}
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("")
+    }
 
     return <div className="home">
     <form onSubmit={handleSearch} className="search-form">
-        <input type="text" placeholder="Search for movies"></input>
+        <input type="text" placeholder="Search for movies" className="search-input" value={searchQuery}
+        onChange={(e)=>setSearchQuery(e.target.value)}></input>
+   
+   <button type="submit" className="search-button">Search</button>
     </form>
 
 <div className="movies-grid">
