7aeb7cfa08b59c47319f32c295df13cfe76a77e9
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-29 22:36:59 +0500

    Update index.html and style.css

--- frontend/src/components/movieCard.jsx
+++ frontend/src/components/movieCard.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import {MovieCard}  from './components/movieCard';
 function App() {
+    const movieNumber=2;
   return (
     <>
+(movieNumber===1 ?(
+
+)
 
 <MovieCard movie={{title:"tim's", release_date:"2025"}} />
+)
 <MovieCard movie={{title:"Joe's", release_date:"2026"}} />
 
 
