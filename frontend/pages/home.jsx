e5a368d5b27683dec07043e223ef7c099328b712
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-29 23:36:19 +0500

    Update index.html and style.css

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -5,8 +5,14 @@ function Home(){
         {id:1, title:"John Wick", release_date:"2023"},
         {id:1, title:"John hilton", release_date:"2020"},
         {id:1, title:"Michael Wick", release_date:"2013"},
-    ]
+    ];
+
+    const handleSearch=()=>{}
+
     return <div className="home">
+    <form onSubmit={handleSearch} className="search-form">
+        <input type="text" placeholder="Search for movies"></input>
+    </form>
 
 <div className="movies-grid">
 {movies.map((movie=> <MovieCard movie={movie} key={movie.id}/>))}
